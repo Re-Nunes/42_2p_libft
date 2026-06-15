94abb93a1de7e17dfd938c643ecd18c2160a1ca4
Re-Nunes <reenunes.and@gmail.com>
2026-06-15 14:40:17 -0300

    Primeiro commit - projeto da parte 2 da Libft

---
+++ ft_substr.c
@@ -0,0 +1,29 @@
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*sub;
+	unsigned int	len_sub;
+	unsigned int	len_s;
+	unsigned int	i;
+
+	if (!s)
+		return (NULL);
+	len_s = ft_strlen(s);
+	if (start >= len_s)
+		return (ft_strdup(""));
+	len_sub = len_s - start;
+	if (len_sub > len)
+		len_sub = len;
+	sub = malloc ((len_sub + 1) * sizeof(char));
+	if (!sub)
+		return (NULL);
+	i = 0;
+	while (i < len_sub)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
